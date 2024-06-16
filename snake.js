b8a6387c2ae5dc577eeb95e347c5fc51dad62568
daashreni <135640722+daashreni@users.noreply.github.com>
2024-06-16 18:19:17 +0545

    changes

---
+++ snake.js
@@ -0,0 +1,25 @@
+// cconstant and variables
+let direction = {x:0, y:0};
+//currently it's in constant position before the game
+const foodSound = new Audio('food.mp3');
+const gameoverSound = new Audio('gameover.mp3');
+const musicSound = new Audio('music.mp3');
+const moveSound = new Audio('move.mp3');
+
+// game function
+function main(cutime){
+    Window.requestAnimationFrame(main);
+    // here main has become a game loop
+    
+}
+
+
+
+
+
+
+
+
+
+// game main logic .........
+c
